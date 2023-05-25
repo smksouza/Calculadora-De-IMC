26e5a1a899f2d454a67f17767d410e8a96336bea
Samuel Souza <smksouza.dev@gmail.com>
2023-05-25 16:01:31 -0300

    projeto imc finalizado ultilizando export e import

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+export const Modal = {
+  wrapper: document.querySelector(".modal-wrapper"),
+  message: document.querySelector(".modal .title span"),
+  btnClose: document.querySelector(".modal button.close"),
+
+  open: function () {
+    Modal.wrapper.classList.add("open")
+  },
+  close: function () {
+    Modal.wrapper.classList.remove("open")
+  },
+}
+
+Modal.btnClose.onclick = function () {
+  Modal.close()
+}
+
+window.addEventListener("keydown", handleKeydown)
+
+function handleKeydown(event) {
+  if (event.key === "Escape") {
+    Modal.close()
+  }
+}
